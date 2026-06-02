8b4fb78ae866e90f92708bd4f8775dcb3de0b8a5
Rivmo <riveristerence@gmail.com>
2026-06-02 17:21:41 -0400

    Debloat
    There was a lot of redundant nodes & I also renamed some stuff

--- main_menu_screen.gd
+++ main_menu_screen.gd
@@ -3,7 +3,7 @@ extends Control
 
 # all buttons
 func _on_practice_pressed() -> void:
-	get_tree().change_scene_to_file("res://scene.tscn") 
+	get_tree().change_scene_to_file("res://testing_platform.tscn") 
 
 func _on_settings_pressed() -> void:
 	get_tree().change_scene_to_file("res://settings_menu_screen.tscn")
