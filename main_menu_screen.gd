3d24e5afcfe9497bc3f0402eff76f91bfea89eef
Rivmo <riveristerence@gmail.com>
2026-06-02 16:49:47 -0400

    Work on GUI
    Added settings menu and moved some scripts

---
+++ main_menu_screen.gd
@@ -0,0 +1,9 @@
+extends Control
+
+
+
+func _on_practice_pressed() -> void:
+	get_tree().change_scene_to_file("res://scene.tscn")
+
+func _on_settings_pressed() -> void:
+	get_tree().change_scene_to_file("res://settings_menu_screen.tscn")
