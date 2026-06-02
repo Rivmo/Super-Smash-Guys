f8234a4f2d38bfb221e9a80994038dd5a0ed82cf
thunderbolt008 <thunderboltjohn323@gmail.com>
2026-06-02 13:42:29 -0400

    Double Jumping
    Added double jumping to character

--- character_body_2d.gd
+++ character_body_2d.gd
@@ -3,15 +3,20 @@ extends CharacterBody2D
 @export var knockback = 0
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
-#skldsfkjlj
+var Double_jump : bool = false
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("Jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	if Input.is_action_just_pressed("Jump"):
+		if is_on_floor():
+			velocity.y = JUMP_VELOCITY
+			Double_jump = false
+		elif not Double_jump:
+			velocity.y = JUMP_VELOCITY
+			Double_jump = true
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
