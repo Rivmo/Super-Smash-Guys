aa529ee8e28772c95c2aa049300064fd5fb5f31b
thunderbolt008 <thunderboltjohn323@gmail.com>
2026-06-02 13:37:45 -0400

    test2
    test2

--- character_body_2d.gd
+++ character_body_2d.gd
@@ -3,7 +3,7 @@ extends CharacterBody2D
 @export var knockback = 0
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
-
+#skldsfkjlj
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
