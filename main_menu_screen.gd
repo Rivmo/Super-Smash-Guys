9fca3ab17ab58ff5dfdbaa85be8e35cc13d5145b
Goofus <james.kirkpatrick421@gmail.com>
2026-06-02 17:42:01 -0400

    Folder-ifyed stuff
    put scenes and images into there own respective new folders in res://

--- main_menu_screen.gd
+++ main_menu_screen.gd
@@ -3,7 +3,7 @@ extends Control
 
 # all buttons
 func _on_practice_pressed() -> void:
-	get_tree().change_scene_to_file("res://scene.tscn") 
+	get_tree().change_scene_to_file("res://scenes/scene.tscn") 
 
 func _on_settings_pressed() -> void:
-	get_tree().change_scene_to_file("res://settings_menu_screen.tscn")
+	get_tree().change_scene_to_file("res://scenes/main_menu_screen.tscn")
