ba4e659d908e77fb52a6515f69ee7c508231463c
Rivmo <riveristerence@gmail.com>
2026-06-02 19:08:34 -0400

    Fixed all the issues
    Re structured alot

--- main_menu_screen.gd
+++
@@ -1,9 +0,0 @@
-extends Control
-
-
-# all buttons
-func _on_practice_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/scene.tscn") 
-
-func _on_settings_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/main_menu_screen.tscn")
---
+++ scripts/main_menu_screen.gd
@@ -0,0 +1,9 @@
+extends Control
+
+
+# all buttons
+func _on_practice_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/placeholder_arena.tscn") 
+
+func _on_settings_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/settings_menu_screen.tscn")
