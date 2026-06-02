e739c12046ed508bc639262bf7bee7ca9c421e03
Rivmo <riveristerence@gmail.com>
2026-06-02 16:53:05 -0400

    test
    test

--- main_menu_screen.gd
+++ main_menu_screen.gd
@@ -1,9 +1,9 @@
 extends Control
 
 
-
+# all buttons
 func _on_practice_pressed() -> void:
-	get_tree().change_scene_to_file("res://scene.tscn")
+	get_tree().change_scene_to_file("res://scene.tscn") 
 
 func _on_settings_pressed() -> void:
 	get_tree().change_scene_to_file("res://settings_menu_screen.tscn")
