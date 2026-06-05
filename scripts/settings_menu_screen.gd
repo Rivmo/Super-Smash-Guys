1381e5cf5440e8297c4fddef3b5055533c55fde4
Rivmo <riveristerence@gmail.com>
2026-06-05 18:31:16 -0400

    Im working on UI man
    WIP

--- scripts/settings_menu_screen.gd
+++ scripts/settings_menu_screen.gd
@@ -2,10 +2,13 @@ extends Control
 
 
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
 
 func _on_exit_settings_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/main_menu_screen.tscn")
 =======
+=======
+>>>>>>> Stashed changes
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -14,4 +17,7 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+<<<<<<< Updated upstream
+>>>>>>> Stashed changes
+=======
 >>>>>>> Stashed changes
