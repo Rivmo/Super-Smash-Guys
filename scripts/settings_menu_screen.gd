96d61b8f61194d855c1fda2d5da5b5569da5d339
Rivmo <riveristerence@gmail.com>
2026-06-05 18:55:08 -0400

    test1
    no corruption please

--- scripts/settings_menu_screen.gd
+++ scripts/settings_menu_screen.gd
@@ -1,6 +1,11 @@
 extends Control
+<<<<<<< Updated upstream
 func _on_exit_settings_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/main_menu_screen.tscn")
+=======
+
+
+>>>>>>> Stashed changes
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -9,4 +14,7 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+<<<<<<< Updated upstream
 	get_tree().change_scene_to_file("res://scenes/main_menu_screen.tscn")
+=======
+>>>>>>> Stashed changes
