36aaec859878a94052b2239f66f462dca1af7a81
Goofus <james.kirkpatrick421@gmail.com>
2026-06-05 19:27:44 -0400

    random push
    random push making small tweaks

--- scripts/dummy.gd
+++ scripts/dummy.gd
@@ -1,3 +1,6 @@
 extends RigidBody2D
 
 @export var knockback = 0
+
+func _ready() -> void:
+	position = Vector2(640,450)
--- scripts/exit_menu.gd
+++ scripts/exit_menu.gd
@@ -2,3 +2,7 @@ extends Control
 
 func _on_exit_to_main_menu_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/main_menu_screen.tscn")
+
+
+func _on_exit_game_pressed() -> void:
+	get_tree().quit()
