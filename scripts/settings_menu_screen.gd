cbf0834385336a6656f5f3dc61e68638bb51739a
Rivmo <riveristerence@gmail.com>
2026-06-05 18:45:47 -0400

    Im working on UI man
    im gonna expolode

--- scripts/settings_menu_screen.gd
+++ scripts/settings_menu_screen.gd
@@ -1,23 +1,7 @@
 extends Control
 
-
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-
 func _on_exit_settings_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/main_menu_screen.tscn")
-=======
-=======
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -26,16 +10,3 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
