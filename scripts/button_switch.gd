f1a0fb2daaf3772a6829b075245c4c0029ce4ef1
Rivmo <riveristerence@gmail.com>
2026-06-05 20:59:16 -0400

    UI changes
    Improving settings ui, begining multiplayer UI

---
+++ scripts/button_switch.gd
@@ -0,0 +1,13 @@
+extends PanelContainer
+
+@onready var buttontext = $MarginContainer2/SettingsButton
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	buttontext.text = "suhdafkshjahfd"
+	pass 
+
+
+
+func _process(delta: float) -> void:
+	
+	pass
---
+++ scripts/exit_multiplayer.gd
@@ -0,0 +1,8 @@
+extends Button
+
+# THIS IS A PLACEHOLDER SCRIPT, REMOVE LATER
+
+
+
+func Exit_Multiplayer_on_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/Multiplayer_select_screen.tscn")
