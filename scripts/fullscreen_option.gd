fb0b1f5ed586c316570e9d6e9889e2eddbbf5077
Rivmo <riveristerence@gmail.com>
2026-06-06 00:47:00 -0400

    added fullscreen option
    idk if it works bc I couldn't test properly

--- scripts/fullscreen_option.gd
+++ scripts/fullscreen_option.gd
@@ -5,5 +5,7 @@ extends Button
 func _on_pressed() -> void:
 	if button.text == "button.on":
 		button.text = "button.off"
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
 	elif button.text == "button.off":
 		button.text = "button.on"
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
